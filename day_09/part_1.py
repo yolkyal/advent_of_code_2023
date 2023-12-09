0feed5523e05fdac22818ab7a94e8a840b6d44d6
yolkyal <yolkyal@hotmail.co.uk>
2023-12-09 16:32:07 +0000

    Create part_1.py

---
+++ day_09/part_1.py
@@ -0,0 +1,17 @@
+with open(0) as f:
+	lines = [list(map(int, line.strip().split())) for line in f]
+
+total = 0
+for line in lines:
+	extrapolations = [line]	
+	while any(val != 0 for val in extrapolations[-1]):
+		new_ex = []
+		for i in range(len(extrapolations[-1]) - 1):
+			new_ex.append(extrapolations[-1][i+1] - extrapolations[-1][i])
+		extrapolations.append(new_ex)
+		
+	for i in range(len(extrapolations) - 2, -1, -1):
+		extrapolations[i].append(extrapolations[i][-1] + extrapolations[i+1][-1])
+	
+	total += extrapolations[0][-1]
+print(total)
